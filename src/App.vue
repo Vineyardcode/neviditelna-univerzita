77798e7a31f7b1da07cc58e06fe24b4556e2e21d
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-07-02 03:49:42 +0200

    master

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app id="inspire">
     <v-app-bar
-        color="blue"
-        dark
+        color="info"
+
       >
         <v-app-bar-nav-icon variant="text"></v-app-bar-nav-icon>
 
@@ -10,72 +10,95 @@
 
         <v-spacer></v-spacer>
 
-        <v-btn
-        class="ma-2"
-      >
-        <v-icon
+    <v-btn color="white" >
+      <v-icon
           start
           icon="mdi-account-circle"
         ></v-icon>
-        {{userName}}
-      </v-btn>
+      {{userName}}
+
+      <v-menu activator="parent">
+          <v-list>
+            <v-list-item title prepend-icon="mdi-account-circle" subtitle="admin">
+                <v-list-item-title>{{ userName }}</v-list-item-title>
+              </v-list-item>
+          </v-list>
+          <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+    </v-btn>
 
 
-
-      </v-app-bar>
+    </v-app-bar>
 
     <v-main class="bg-grey-lighten-3">
-      <v-container>
-        <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list rounded="lg">
-                <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  link
-                >
-                  <v-list-item-title>
-                    List Item {{ n }}
-                  </v-list-item-title>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item
-                  link
-                  color="grey-lighten-4"
-                >
-                  <v-list-item-title>
-                    Refresh
-                  </v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
-
-          <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
+      <v-navigation-drawer
+        permanent
+        location="left"
+      >
+      <v-col>
+        <v-sheet >
+          <v-list >
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              link
+              color="yellow-darken-2"
             >
-             MAIN aaa
-            </v-sheet>
+              <v-list-item-title>
+                List Item {{ n }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-sheet>
+      </v-col>
+    </v-navigation-drawer>
+
+        <v-container >
+          <v-col >
+            <v-switch
+              color="info"
+              :model-value="true"
+              label="Verbose view"
+            ></v-switch>
+
+
+            <v-card class="mx-auto" max-width="400">
+              <v-toolbar>
+                <v-card-title>Events: </v-card-title>
+              </v-toolbar>
+              <v-card-text>
+                <div>Whitehaven Beach</div>
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+            </v-card>
           </v-col>
-        </v-row>
-      </v-container>
+        </v-container>
+
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
+  data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'LogOut' },
+      ],
       drawer: true,
       userName: 'Jan Musílek',
-    };
-  },
+    }),
   methods: {
     logout() {
       // odlogovat usera
